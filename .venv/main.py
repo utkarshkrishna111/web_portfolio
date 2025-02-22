1118a437c033d457b1064aea9c67a16d782b04af
Utkarsh Krishna <utkarsh_krishna@outlook.com>
2025-02-22 23:33:51 +0100

    Day 22: Render the title

--- .venv/main.py
+++ .venv/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,3 +16,14 @@ with col2:
 content2 = """ Below you can find some of the applications I have built using Python. Feel free to contact me! """
 st.write(content2)
 
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv(".venv/data.csv", sep= ";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
